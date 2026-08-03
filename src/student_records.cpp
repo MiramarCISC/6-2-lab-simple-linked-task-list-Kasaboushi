098ce78deda5400c3cf6d11b010e02ee2d1efb81
tqassar <tqassar@gmail.com>
2026-08-02 20:30:11 -0700

    added comments

--- src/student_records.cpp
+++ src/student_records.cpp
@@ -4,16 +4,13 @@
 using namespace std;
 
 bool isValidStudentId(string id) {
-    if (id.length() == 0) {
+    if (id.length() == 0) { // consider combining these two if statements
         return false;
     }
     if (id.length() < 3) {
         return false;
     }
-    if (id[0] < 'A' || id[0] > 'Z') {
-        return false;
-    }
-    if (id[0] < 'A' || id[0] > 'Z') {
+    if (id[0] < 'A' || id[0] > 'Z') { //double logic should be removed. This doesn't seem useful, so I'll take out the second if.
         return false;
     }
     // TODO:// A valid ID should have at least 3 characters.// A valid ID should start with an uppercase letter.
@@ -23,25 +20,25 @@ bool isValidStudentId(string id) {
 
 bool isValidScore(double score) {
     // TODO: Return true when score is between 0 and 100, inclusive.
-    return score >= 0 && score <= 100;;
+    return score >= 0 && score <= 100;; // double ; is not wrong, but definitely not needed
 }
 
 void printStudent(const Student& student) {
     cout << student.id << " "
          << student.name << " "
          << student.score << " "
-         << determineLetterGrade(student.score)
+         << determineLetterGrade(student.score) //good multi-line cout
          << endl;
 }
 
 void printStudents(const Student students[], int size) {
     if (students == nullptr || size <= 0) {
-        cout << "No students to print." << endl;
+        cout << "No students to print." << endl; //no need for a return statement here
         return;
     }
 
     for (int i = 0; i < size; i++) {
-        printStudent(students[i]);
+        printStudent(students[i]); 
     }
 }
 
@@ -49,15 +46,15 @@ double calculateAverageScore(const Student students[], int size) {
     if (students == nullptr || size <= 0) {
         return 0.0;
     }
-    double total = 0;
+    double total = 0; //since you'll return 0 anyway, you could simply return total and place this variable at the top.
 
     for (int i = 0; i < size; i++) {
         total += students[i].score;
     }
-    // TODO:// If the array is null or size is invalid, return 0.0.// Otherwise, return the average score.
+    
     return total / size;
 }
-
+// TODO:// If the array is null or size is invalid, return 0.0.// Otherwise, return the average score.
 double findHighestScore(const Student students[], int size) {
     if (students == nullptr || size <= 0) {
         return 0.0;
@@ -77,7 +74,7 @@ double findHighestScore(const Student students[], int size) {
 int findStudentById(const Student students[], int size, string targetId) {
     
     if (students == nullptr || size <=0) {
-        return -1;
+        return -1; //works in this specific constraint
     }
 
     for (int i = 0; i < size; i++) {
